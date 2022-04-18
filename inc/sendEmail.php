65e9fb4f001667a8d6310e5f76b5ebc6e53e174e
AnastaciusWright <alexandronadal@gmail.com>
2022-04-18 11:16:43 +0200

    working emailer

--- inc/sendEmail.php
+++ inc/sendEmail.php
@@ -1,13 +1,24 @@
-﻿<?php
-
-// Replace this with your own email address
-$siteOwnersEmail = 'user@website.com';
+﻿<?php require("class.phpmailer.php");
+require("class.smtp.php");
+include("pass.php");
 
 
 if($_POST) {
-
+   $mail = new PHPMailer();
+   $mail->IsSMTP();
+   $mail->Host = "mail.alejandronadal.com";
+   $mail->SMTPAuth = true;
+   $mail->Username = "mailer@alejandronadal.com";
+   $mail->Password = $emailPass;
+   $mail->WordWrap = 50;
+   $mail->isHTML(true);
+   $mail->From ="mailer@alejandronadal.com";
+   $mail->FromName="Mailer";
    $name = trim(stripslashes($_POST['contactName']));
-   $email = trim(stripslashes($_POST['contactEmail']));
+   $replyAddress = trim(stripslashes($_POST['contactEmail']));
+   $mail->AddAddress("alexandronadal@gmail.com");
+   $mail->AddReplyTo($replyAddress, "Informacion");
+   
    $subject = trim(stripslashes($_POST['contactSubject']));
    $contact_message = trim(stripslashes($_POST['contactMessage']));
 
@@ -16,7 +27,7 @@ if($_POST) {
 		$error['name'] = "Please enter your name.";
 	}
 	// Check Email
-	if (!preg_match('/^[a-z0-9&\'\.\-_\+]+@[a-z0-9\-]+\.([a-z0-9\-]+\.)*+[a-z]{2}/is', $email)) {
+	if (!preg_match('/^[a-z0-9&\'\.\-_\+]+@[a-z0-9\-]+\.([a-z0-9\-]+\.)*+[a-z]{2}/is', $replyAddress)) {
 		$error['email'] = "Please enter a valid email address.";
 	}
 	// Check Message
@@ -27,42 +38,37 @@ if($_POST) {
 	if ($subject == '') { $subject = "Contact Form Submission"; }
 
 
-   // Set Message
-   $message .= "Email from: " . $name . "<br />";
+	// Set Message
+   	$message .= "Email from: " . $name . "<br />";
 	$message .= "Email address: " . $email . "<br />";
-   $message .= "Message: <br />";
-   $message .= $contact_message;
-   $message .= "<br /> ----- <br /> This email was sent from your site's contact form. <br />";
-
-   // Set From: header
-   $from =  $name . " <" . $email . ">";
-
-   // Email Headers
-	$headers = "From: " . $from . "\r\n";
-	$headers .= "Reply-To: ". $email . "\r\n";
- 	$headers .= "MIME-Version: 1.0\r\n";
-	$headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
+	$message .= "Message: <br />";
+	$message .= $contact_message;
+	$message .= "<br /> ----- <br /> This email was sent from your site's contact form. <br />";
 
 
    if (!$error) {
 
-      ini_set("sendmail_from", $siteOwnersEmail); // for windows server
-      $mail = mail($siteOwnersEmail, $subject, $message, $headers);
+	$mail->Subject = $subject;
+	$mail->Body = $message;
 
-		if ($mail) { echo "OK"; }
-      else { echo "Something went wrong. Please try again."; }
+	if ($mail->Send()) 
+	{ 
+		echo "OK"; 
+	}
+	else { 
+		echo "Something went wrong. Please try again."; 
+	}
 		
-	} # end if - no validation error
+   } # end if - no validation error
+   else {
 
-	else {
+	$response = (isset($error['name'])) ? $error['name'] . "<br /> \n" : null;
+	$response .= (isset($error['email'])) ? $error['email'] . "<br /> \n" : null;
+	$response .= (isset($error['message'])) ? $error['message'] . "<br />" : null;
+	
+	echo $response;
 
-		$response = (isset($error['name'])) ? $error['name'] . "<br /> \n" : null;
-		$response .= (isset($error['email'])) ? $error['email'] . "<br /> \n" : null;
-		$response .= (isset($error['message'])) ? $error['message'] . "<br />" : null;
-		
-		echo $response;
-
-	} # end if - there was a validation error
+   } # end if - there was a validation error
 
 }
 
